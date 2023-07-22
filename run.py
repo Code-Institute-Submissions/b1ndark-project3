d91337911ae88a96e9e99af9b6228faaa49a30c9
b1ndark <>
2023-07-22 20:25:20 +0000

    add mainMenu function with try/except and if statements for the options

--- run.py
+++ run.py
@@ -1,8 +1,14 @@
-# import random so cards can be shuffled in the game
+# import random, so cards can be shuffled in the game
 import random
+import os
+
+
+def instructions():
+    print("Hello")
 
 
 # Variables
+value = False
 cards = []
 card_suits = ["clubs", "diamonds", "hearts", "spades"]
 ranks = [
@@ -27,5 +33,37 @@ for card_suit in card_suits:
         cards.append([card_suit, rank])
 
 # To shuffle the cards list
-random.shuffle(cards)
-print(cards[3])
+def shuffleCard():
+    random.shuffle(cards)
+
+def dealCard(number):
+    cardsDealt = []
+    card = cards.pop()
+    return cardsDealt
+
+
+
+# Main Menu function where you will be able to select Instructions or start the game
+def mainMenu():
+    print("*" * 40)
+    print("\n        Welcome to BlackJack Game \n")
+    print("*" * 40)
+    print("*" * 40)
+    print("\nPlease select from the following options:")
+    number_entered = input("\n   1: Instructions.\n   2: Start.\n")
+    # Try/Except to check whether is a number or not
+    try:
+        number_entered = int(number_entered)
+    except:
+        print("Please enter a number")
+        number_entered = int(input("\n   1: Instructions.\n   2: Start.\n"))
+    # If statement to check what option user has selected
+    if number_entered == 1:
+        instructions()
+    elif number_entered == 2:
+        username()
+    else:
+        print("Please enter 1 or 2")    
+    
+mainMenu()
+
