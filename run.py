9a35e26d54044c0e83a5076a65aac70f12d5705a
b1ndark <>
2023-07-22 14:52:23 +0000

    append card_suits and ranks to cards list, add shuffle to cards list

--- run.py
+++ run.py
@@ -1,6 +1,31 @@
+# import random so cards can be shuffled in the game
 import random
 
-cards = []
-ranks = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
-print(ranks)
+# Variables
+cards = []
+card_suits = ["clubs", "diamonds", "hearts", "spades"]
+ranks = [
+    {"rank": "2", "value": 2},
+    {"rank": "3", "value": 3},
+    {"rank": "4", "value": 4},
+    {"rank": "5", "value": 5},
+    {"rank": "6", "value": 6},
+    {"rank": "7", "value": 7},
+    {"rank": "8", "value": 8},
+    {"rank": "9", "value": 9},
+    {"rank": "10", "value": 10},
+    {"rank": "J", "value": 10},
+    {"rank": "Q", "value": 10},
+    {"rank": "K", "value": 10},
+    {"rank": "A", "value": 11},
+]
+
+# Append card_suits and ranks onto cards list 'cards = []'
+for card_suit in card_suits:
+    for rank in ranks:
+        cards.append([card_suit, rank])
+
+# To shuffle the cards list
+random.shuffle(cards)
+print(cards[3])
